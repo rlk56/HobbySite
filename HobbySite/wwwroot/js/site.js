86f270e923aeb95068ab88197c857a3a9e0e5f57
Rachel <rlk56@byu.edu>
2024-02-07 15:34:53 -0700

    Add project files.

---
+++ HobbySite/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = parseInt($('#hours').val());
+        var rate = parseInt($('#rate').val().substring(1)); // Remove '$' and parse as integer
+        if (!isNaN(hours) && hours > 0) {
+            var total = hours * rate;
+            $('#total').val('$' + total);
+        } else {
+            alert('Please enter a valid positive number of hours.');
+        }
+    });
+});
